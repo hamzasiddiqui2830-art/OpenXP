ab9f9db2b24c6cfa91849bbb811bc1d627c39d4d
hamzasiddiqui2830-art <hamzasiddiqui2830@gmail.com>
2026-09-09 08:07:16 +0500

    Remove host winternl dependency from Zw API

--- ntoskrnl/inc/zwapi.h
+++ ntoskrnl/inc/zwapi.h
@@ -6,9 +6,9 @@ Module Name:
 
 Abstract:
 
-    Compatibility entry point for private RTL code that includes the WRK
-    Zw API declarations. Keep this header intentionally dependency-light;
-    the host native SDK provides the common declarations when available.
+    Compatibility entry point for private WRK Zw API declarations.
+    OpenXP supplies the complete NT declaration set locally; this header
+    therefore must not include the host Windows SDK native API headers.
 
 --*/
 
@@ -16,9 +16,5 @@ Abstract:
 #define _ZWAPI_COMPAT_
 
 #include "nt.h"
-
-#if defined(_MSC_VER)
-#include <winternl.h>
-#endif
 
 #endif /* _ZWAPI_COMPAT_ */

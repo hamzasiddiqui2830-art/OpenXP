d286f98bc7b78007245fb5e371bd519972b83ab2
hamzasiddiqui2830-art <hamzasiddiqui2830@gmail.com>
2026-09-11 11:23:03 +0500

    Fix forced WRK compatibility header include ordering

--- ntoskrnl/inc/wrk_msvc_compat.h
+++ ntoskrnl/inc/wrk_msvc_compat.h
@@ -1,6 +1,16 @@
 #ifndef _WRK_MSVC_COMPAT_H_
 #define _WRK_MSVC_COMPAT_H_
 
+/*
+ * _NTOS_ is also the include guard used by ntos.h.  The WRK compiler
+ * environment may define _NTOS_ as a command-line symbol, so clear it
+ * before the forced include of ntos.h.  Otherwise ntos.h is skipped and
+ * architecture types such as PFN_NUMBER are unavailable when mi386.h is
+ * parsed below.
+ */
+#ifdef _NTOS_
+#undef _NTOS_
+#endif
 #include "ntos.h"
 
 #ifdef NTHALAPI
@@ -182,139 +192,6 @@ extern ULONG MiDetermineUserGlobalPteMask(IN PMMPTE PointerPte);
 #endif
 #endif
 
-#ifndef COMPLUS_PACKAGE_KEYPATH
-#define COMPLUS_PACKAGE_KEYPATH L"\\Registry\\Machine\\SOFTWARE\\Microsoft\\.NETFramework"
-#endif
-#ifndef COMPLUS_PACKAGE_ENABLE64BIT
-#define COMPLUS_PACKAGE_ENABLE64BIT L"Enable64Bit"
-#endif
-#ifndef COMPLUS_PACKAGE_INVALID
-#define COMPLUS_PACKAGE_INVALID ((ULONG)-1)
-#endif
-#ifndef _WRK_COMPLUS_PACKAGE_ROUTINES_DECLARED
-#define _WRK_COMPLUS_PACKAGE_ROUTINES_DECLARED
-NTSTATUS ExpReadComPlusPackage(VOID);
-NTSTATUS ExpUpdateComPlusPackage(IN ULONG ComPlusPackageStatus);
-#endif
-
-#if defined(_MSC_VER) && defined(_X86_)
-#pragma warning(disable: 4142)
-#endif
-
-#include "cmdata.h"
-
-#ifndef _WRK_CM_HASH_TABLE_ENTRIES_DEFINED
-#define _WRK_CM_HASH_TABLE_ENTRIES_DEFINED
-typedef struct _CM_KEY_HASH_TABLE_ENTRY { EX_PUSH_LOCK Lock; PKTHREAD Owner; PCM_KEY_HASH Entry; } CM_KEY_HASH_TABLE_ENTRY, *PCM_KEY_HASH_TABLE_ENTRY;
-typedef struct _CM_NAME_HASH_TABLE_ENTRY { EX_PUSH_LOCK Lock; PCM_NAME_HASH Entry; } CM_NAME_HASH_TABLE_ENTRY, *PCM_NAME_HASH_TABLE_ENTRY;
-#endif
-
-#ifndef _WRK_EXP_WATCH_EXPIRATION_DATA_WORK_DECLARED
-#define _WRK_EXP_WATCH_EXPIRATION_DATA_WORK_DECLARED
-static VOID ExpWatchExpirationDataWork(IN PVOID Context);
-#endif
-#ifndef ProbeForWriteUlongAligned32
-#define ProbeForWriteUlongAligned32(_Address) ProbeForWriteSmallStructure((PVOID)(_Address), sizeof(ULONG), sizeof(ULONG))
-#endif
-#ifndef _WRK_EX_RUNDOWN_REF_CACHE_AWARE_DEFINED
-#define _WRK_EX_RUNDOWN_REF_CACHE_AWARE_DEFINED
-typedef struct _EX_RUNDOWN_REF_CACHE_AWARE { PEX_RUNDOWN_REF RunRefs; PVOID PoolToFree; ULONG RunRefSize; ULONG Number; } EX_RUNDOWN_REF_CACHE_AWARE, *PEX_RUNDOWN_REF_CACHE_AWARE;
-#endif
-#ifndef _WRK_EX_RUNDOWN_CACHE_AWARE_API_DECLARED
-#define _WRK_EX_RUNDOWN_CACHE_AWARE_API_DECLARED
-PEX_RUNDOWN_REF_CACHE_AWARE ExAllocateCacheAwareRundownProtection(IN POOL_TYPE PoolType, IN ULONG PoolTag);
-SIZE_T ExSizeOfRundownProtectionCacheAware(VOID);
-VOID ExInitializeRundownProtectionCacheAware(IN PEX_RUNDOWN_REF_CACHE_AWARE RunRefCacheAware, IN SIZE_T Size);
-VOID ExFreeCacheAwareRundownProtection(IN PEX_RUNDOWN_REF_CACHE_AWARE RunRefCacheAware);
-#endif
-#ifndef MAX_PAGE_FILES
-#define MAX_PAGE_FILES 16
-#endif
-#ifndef MM_MINIMUM_VA_FOR_LARGE_PAGE
-#define MM_MINIMUM_VA_FOR_LARGE_PAGE 0x400000UL
-#endif
-#ifndef MM_PTE_EXECUTE
-#define MM_PTE_EXECUTE 0x0UL
-#endif
-#ifndef MM_PTE_EXECUTE_READ
-#define MM_PTE_EXECUTE_READ 0x0UL
-#endif
-#ifndef MM_PTE_EXECUTE_READWRITE
-#define MM_PTE_EXECUTE_READWRITE 0x800UL
-#endif
-#ifndef MM_PTE_EXECUTE_WRITECOPY
-#define MM_PTE_EXECUTE_WRITECOPY 0x200UL
-#endif
-#ifndef GET_PAGING_FILE_NUMBER
-#define GET_PAGING_FILE_NUMBER(_PteContents) ((_PteContents).u.Soft.PageFileLow)
-#endif
-#ifndef GET_PAGING_FILE_OFFSET
-#define GET_PAGING_FILE_OFFSET(_PteContents) ((_PteContents).u.Soft.PageFileHigh)
-#endif
-#ifndef MI_WRITE_ZERO_PTE
-#define MI_WRITE_ZERO_PTE(_PointerPte) RtlZeroMemory((_PointerPte), sizeof(MMPTE))
-#endif
-#ifndef MI_WRITE_VALID_PTE
-#define MI_WRITE_VALID_PTE(_PointerPte, _TempPte) (*(_PointerPte) = (_TempPte))
-#endif
-#ifndef MI_WRITE_INVALID_PTE
-#define MI_WRITE_INVALID_PTE(_PointerPte, _TempPte) (*(_PointerPte) = (_TempPte))
-#endif
-#ifndef MI_SET_PFN_DELETED
-#define MI_SET_PFN_DELETED(_Pfn) ((_Pfn)->PteAddress = (PMMPTE)((ULONG_PTR)(_Pfn)->PteAddress | 1))
-#endif
-#ifndef MI_MAKE_VALID_PTE_TRANSITION
-#define MI_MAKE_VALID_PTE_TRANSITION(_Pte, _Protect) do { (_Pte).u.Soft.Transition = 1; (_Pte).u.Soft.Valid = 0; (_Pte).u.Soft.Prototype = 0; (_Pte).u.Soft.Protection = (_Protect); } while (0)
-#endif
-#ifndef MI_CAPTURE_DIRTY_BIT_TO_PFN
-#define MI_CAPTURE_DIRTY_BIT_TO_PFN(_PointerPte, _Pfn) ((void)0)
-#endif
-#ifndef MI_IS_PHYSICAL_ADDRESS
-#define MI_IS_PHYSICAL_ADDRESS(_Address) (FALSE)
-#endif
-#if defined(_X86_)
-#ifndef SecondaryColorMask
-#define SecondaryColorMask PageColor
-#endif
-#ifndef NodeShiftedColor
-#define NodeShiftedColor PageColor
-#endif
-#endif
-#ifndef KeLoopTbFlushTimeStampUnlocked
-static __inline VOID KeLoopTbFlushTimeStampUnlocked(VOID) { while (KeReadTbFlushTimeStamp() & 1) { YieldProcessor(); } }
-#endif
-#ifndef WRK_MM_CHECK_SYSTEM_IMAGE_DECLARED
-#define WRK_MM_CHECK_SYSTEM_IMAGE_DECLARED
-NTSTATUS MmCheckSystemImage(
-    IN HANDLE ImageFileHandle,
-    IN LOGICAL PurgeSection
-    );
-#endif
-#ifndef WRK_MM_LOCK_PAGEABLE_SECTION_BY_HANDLE_DECLARED
-#define WRK_MM_LOCK_PAGEABLE_SECTION_BY_HANDLE_DECLARED
-VOID MmLockPageableSectionByHandle(IN PVOID ImageSectionHandle);
-#endif
-#ifndef WRK_MM_UNLOCK_PAGEABLE_IMAGE_SECTION_DECLARED
-#define WRK_MM_UNLOCK_PAGEABLE_IMAGE_SECTION_DECLARED
-VOID MmUnlockPageableImageSection(IN PVOID ImageSectionHandle);
-#endif
-#ifndef QuantumReset
-#define QuantumReset Spare4
-#endif
-#ifndef Hand
-#define Hand Size
-#endif
-#ifndef OBJ_VALID_PRIVATE_ATTRIBUTES
-#define OBJ_VALID_PRIVATE_ATTRIBUTES 0x00010000L
-#endif
-#ifndef OBJ_ALL_VALID_ATTRIBUTES
-#define OBJ_ALL_VALID_ATTRIBUTES OBJ_VALID_ATTRIBUTES
-#endif
-#ifndef OBJ_KERNEL_EXCLUSIVE
-#define OBJ_KERNEL_EXCLUSIVE 0x00010000L
-#endif
-#ifndef ProbeAndReadUnicodeStringEx
-FORCEINLINE VOID ProbeAndReadUnicodeStringEx(OUT PUNICODE_STRING Destination, IN PUNICODE_STRING Source) { ProbeForRead(Source, sizeof(UNICODE_STRING), sizeof(ULONG)); *Destination = *Source; }
-#endif
+NTSTATUS MmCheckSystemImage(IN HANDLE ImageFileHandle, IN LOGICAL PurgeSection);
 
 #endif /* _WRK_MSVC_COMPAT_H_ */

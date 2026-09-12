da8a7a1bdfaf83160c2a796ec970140a57235b36
hamzasiddiqui2830-art <hamzasiddiqui2830@gmail.com>
2026-09-12 18:31:19 +0500

    Remove unused variable ZeroPteArgument
    Removed unused variable ZeroPteArgument in mmfault.c.

--- ntoskrnl/mm/mmfault.c
+++ ntoskrnl/mm/mmfault.c
@@ -1745,7 +1745,6 @@ UserFault:
                 (!MiEnsureAvailablePageOrWait (CurrentProcess, OldIrql))) {
 
                 ULONG Color;
-                PMMPTE ZeroPteArgument;
                 Color = MI_PAGE_COLOR_VA_PROCESS (VirtualAddress,
                                                 &CurrentProcess->NextPageColor);
                 PageFrameIndex = MiRemoveZeroPageIfAny (Color);

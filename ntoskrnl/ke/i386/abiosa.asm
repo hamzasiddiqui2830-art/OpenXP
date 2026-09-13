22e095189d600693bf17c07bca60bfd6022dea4f
hamzasiddiqui2830-art <hamzasiddiqui2830@gmail.com>
2026-09-13 20:39:48 +0500

    Update abiosa.asm

--- ntoskrnl/ke/i386/abiosa.asm
+++ ntoskrnl/ke/i386/abiosa.asm
@@ -42,13 +42,6 @@ IFDEF DBG
 extrn  KiBiosReenteredAssert:DWORD
 ENDIF
 
-; Macro change note:
-;
-;   This macro pair used to do an unconditional sti coming back from the 16-bit
-;   side, this potentially caused problems in APM. Now we save and restore the
-;   flag state
-;
-
 ;++
 ;
 ;   STACK32_TO_STACK16
@@ -56,10 +49,6 @@ ENDIF
 ;   Macro Description:
 ;
 ;       This macro remaps current 32bit stack to 16bit stack.
-;
-;   Arguments:
-;
-;       None.
 ;
 ;--
 
@@ -73,19 +62,14 @@ STACK32_TO_STACK16      MACRO
         MOV     EAX, [EAX]+ThStackLimit ; get thread stack base
         MOV     EDX, EAX
         MOV     ECX, _KiStack16GdtEntry
-        MOV     WORD PTR [ECX].KgdtBaseLow, AX
+        MOV     WORD PTR [ECX+KgdtBaseLow], AX          ; FIXED: use + offset
         SHR     EAX, 16
-        MOV     BYTE PTR [ECX].KgdtBaseMid, AL
-        MOV     BYTE PTR [ECX].KgdtBaseHi, AH
+        MOV     BYTE PTR [ECX+KgdtBaseMid], AL          ; FIXED: use + offset
+        MOV     BYTE PTR [ECX+KgdtBaseHi], AH           ; FIXED: use + offset
         CLI
         SUB     ESP, EDX
         MOV     EAX, KGDT_STACK16
         MOV     SS, AX
-
-;
-; NOTE that we MUST leave interrupts remain off.
-; We'll turn it back on after we switch to 16 bit code.
-;
 
 ENDM
 
@@ -96,10 +80,6 @@ ENDM
 ;   Macro Description:
 ;
 ;       This macro remaps current 32bit stack to 16bit stack.
-;
-;   Arguments:
-;
-;       None.
 ;
 ;--
 
@@ -159,19 +139,6 @@ _BBT_Exclude_Selector_Code_Begin  EQU     $
 ; KiAbiosGetGdt (
 ;     VOID
 ;     )
-;
-; Routine Description:
-;
-;     This routine returns the starting address of GDT of current processor.
-;
-; Arguments:
-;
-;     None.
-;
-; Return Value:
-;
-;     return Pcr->GDT
-;
 ;--
 
 cPublicProc _KiAbiosGetGdt,0
@@ -180,7 +147,7 @@ cPublicProc _KiAbiosGetGdt,0
         stdRET    _KiAbiosGetGdt
 
 stdENDP _KiAbiosGetGdt
-
+
 ;++
 ; VOID
 ; KiI386CallAbios(
@@ -189,36 +156,6 @@ stdENDP _KiAbiosGetGdt
 ;     IN KABIOS_POINTER FunctionTransferTable,
 ;     IN KABIOS_POINTER RequestBlock
 ;     )
-;
-; Routine Description:
-;
-;     This function invokes ABIOS service function for device driver.  This
-;     routine is executing at DIAPTCH_LEVEL to prevent context swapping.
-;
-;     N.B. We arrive here from the Ke386AbiosCall with a 32bit CS. That is,
-;     we're executing the code with cs:eip where cs contains a selector for a
-;     32bit flat segment. We want to get to a 16bit cs. That is, cs:ip.
-;     The reason is that ABIOS is running at 16 bit segment.
-;     Before we can call ABIOS service we must load ss and cs segment
-;     registers with selectors for 16bit segments.  We start by pushing a far
-;     pointer to a label in the macro and then doing a retf. This allows us
-;     to fall through to the next instruction, but we're now executing
-;     through cs:ip with a 16bit CS. Then, we remap our 32-bit stack to 16-bit
-;     stack.
-;
-; Arguments:
-;
-;     AbiosFunction - a 16:16 pointer to the abios service function.
-;
-;     DeviceBlockPointer - a 16:16 pointer to Device Block.
-;
-;     FunctionTransferTable - a 16:16 pointer to Function Transfer Table.
-;
-;     RequestBlock - a 16:16 pointer to device driver's request block.
-;
-; Return Value:
-;
-;     None.
 ;--
 
 KacAbiosFunction        EQU     [EBP + 8]
@@ -228,56 +165,40 @@ KacRequestBlock         EQU     [EBP + 20]
 
 cPublicProc _KiI386CallAbios,4
 
-;
-; We're using a 32bit CS:EIP - go to a 16bit CS:IP
-; Note the base of KiAbiosCallSelector is the flat address of _KiI386AbiosCall
-; routine.
-;
-
         PUSH    EBP
         MOV     EBP, ESP
         PUSH    EBX
 
         COPY_CALL_FRAME _KiBiosFrame
-        SUB     ESP, LocalStack          ; After C style frame
-        CurrentIrql                             ; Local Variable
-        PUSH    EAX                             ; Local Variable
+        SUB     ESP, LocalStack
+        CurrentIrql
+        PUSH    EAX
 
-        CMP     AL, DISPATCH_LEVEL              ; Is irql > Dispatch_level?
+        CMP     AL, DISPATCH_LEVEL
         JAE     SHORT Kac00
 
-; Raise to Dispatch Level
         RaiseIrql DISPATCH_LEVEL
         MOV     [ESP], AL
 
 Kac00:
 
-;
-; Set up parameters on stack before remapping stack.
-;
+        PUSH    WORD PTR KGDT_CDA16
+        PUSH    KacRequestBlock
+        PUSH    KacFunctionTable
+        PUSH    KacDeviceBlock
+        MOV     EBX, KacAbiosFunction
 
-        PUSH    WORD PTR KGDT_CDA16             ; CDA anchor selector
-        PUSH    KacRequestBlock                 ; Request Block
-        PUSH    KacFunctionTable                ; Func transfer table
-        PUSH    KacDeviceBlock                  ; Device Block
-        MOV     EBX, KacAbiosFunction           ; (ebx)-> Abios Entry
-
-;
-; Remap current stack to 16:16 stack.  The base of the 16bit stack selector is
-; the base of current kernel stack.
-;
-
-        INC     _KiInBiosCall                         ; Set the 'In Bios' flag
+        INC     _KiInBiosCall
 IFDEF DBG
-        CMP   _KiInBiosCall, 2
-        JB  @F
+        CMP     _KiInBiosCall, 2
+        JB      @F
         PUSH    OFFSET FLAT:KiBiosReenteredAssert
         CALL    _DbgPrint
         ADD     ESP, 4
 @@:
 ENDIF
 
-        STACK32_TO_STACK16                      ; Switch to 16bit stack
+        STACK32_TO_STACK16
         PUSH    WORD PTR KGDT_CODE16
         PUSH    WORD PTR (OFFSET FLAT:Kac40 - OFFSET FLAT:_KiI386CallAbios@16)
         PUSH    KGDT_CODE16
@@ -286,24 +207,13 @@ ENDIF
 
 Kac30:
 
-;
-; Stack switching (from 32 to 16) turns interrupt off.  We must turn it
-; back on.
-;
-
         STI
-        PUSH    BX                              ; Yes, BX not EBX!
+        PUSH    BX
         RETF
 Kac40:
-        ADD     ESP, 14                         ; pop out all the parameters
+        ADD     ESP, 14
 
-        STACK16_TO_STACK32                      ; switch back to 32 bit stack
-
-;
-; Pull callers flat return address off stack and push the
-; flat code selector followed by the return offset, then
-; execute a far return and we'll be back in the 32-bit code space.
-;
+        STACK16_TO_STACK32
 
         DB      OPERAND_OVERRIDE
         PUSH    KGDT_R0_CODE
@@ -312,95 +222,57 @@ Kac40:
         DB      OPERAND_OVERRIDE
         RETF
 Kac50:
-        POP     ECX                             ; [ecx] = OldIrql
-        POP     EBX                             ; restore ebx
+        POP     ECX
+        POP     EBX
         CMP     CL, DISPATCH_LEVEL
         JAE     SHORT Kac60
 
-        LowerIrql CL
+        LowerIrql cl                  ; FIXED: lowercase 'cl' to match IFIDN in macro
 
 Kac60:
 
-        DEC     _KiInBiosCall                          ;Clear 'In Bios' Flag
+        DEC     _KiInBiosCall
 
-        ADD     ESP, LocalStack                           ; subtract off the scratch space
+        ADD     ESP, LocalStack
         POP     EBP
         stdRET    _KiI386CallAbios
 
 stdENDP _KiI386CallAbios
 
 
-;; ********************************************************
-;;
-;; BEGIN - power_management
-;;
-;;
-
 ;++
 ; VOID
 ; KeI386Call16BitFunction (
 ;     IN OUT PCONTEXT Regs
 ;     )
-;
-; Routine Description:
-;
-;     This function calls the 16 bit function specified in the Regs.
-;
-; Parameters:
-;
-;     Regs - supplies a pointer to register context to call 16 function.
-;
-;   NOTE: Caller must be at DPC_LEVEL
-;
 ;--
 
 cPublicProc _KeI386Call16BitFunction,1
 
-    ;  verify CurrentIrql
-    ;  verify context flags
-
-        PUSH    EBP                             ; save nonvolatile registers
+        PUSH    EBP
         PUSH    EBX
         PUSH    ESI
         PUSH    EDI
-        MOV     EBX, DWORD PTR [ESP + 20]       ; (ebx)-> Context
+        MOV     EBX, DWORD PTR [ESP + 20]
 
         COPY_CALL_FRAME _KiBiosFrame
 
-        SUB     ESP, LocalStack          ; After prolog
+        SUB     ESP, LocalStack
 
-        INC    _KiInBiosCall                         ; Set the 'In Bios' flag
+        INC     _KiInBiosCall
 IFDEF DBG
-        CMP   _KiInBiosCall, 2
-        JB  @F
+        CMP     _KiInBiosCall, 2
+        JB      @F
         PUSH    OFFSET FLAT:KiBiosReenteredAssert
         CALL    _DbgPrint
         ADD     ESP, 4
 @@:
 ENDIF
 
-;
-; We're using a 32bit CS:EIP - go to a 16bit CS:IP
-; Note the base of KiAbiosCallSelector is the flat address of _KiI386AbiosCall
-; routine.
-;
-
-;
-; Remap current stack to 16:16 stack.  The base of the 16bit stack selector is
-; the base of current kernel stack.
-;
-
-        STACK32_TO_STACK16                      ; Switch to 16bit stack
-    ;
-    ; Push return address from 16 bit function call to kernel
-    ;
+        STACK32_TO_STACK16
 
         PUSH    WORD PTR KGDT_CODE16
         PUSH    WORD PTR (OFFSET FLAT:Kbf40 - OFFSET FLAT:_KiI386CallAbios@16)
-
-        ;
-        ; Load context to call with
-        ;
 
         PUSH    WORD PTR [EBX + CsEFlags]
         PUSH    WORD PTR [EBX + CsSegCs]
@@ -422,24 +294,14 @@ ENDIF
         POP     FS
         POP     GS
 
-    ;
-    ; Switch to 16bit CS
-    ;
         PUSH    KGDT_CODE16
         PUSH    OFFSET FLAT:Kbf30 - OFFSET FLAT:_KiI386CallAbios@16
         RETF
 
 Kbf30:
-    ;
-    ; "call" to 16 bit function
-    ;
         IRETD
 
 Kbf40:
-    ;
-    ; Push some of the returned context which will be needed to
-    ; switch back to the 32 bit SS & CS.
-    ;
         DB      OPERAND_OVERRIDE
         PUSH    DS
 
@@ -469,17 +331,7 @@ Kbf40:
 
         XOR     EAX, EAX
 
-    ;
-    ; Switch back to 32 bit stack
-    ;
-
         STACK16_TO_STACK32
-
-;
-; Push the flat code selector followed by the return offset, then
-; execute a far return and we'll be back in the 32-bit code space.
-;
-
 
         DB      OPERAND_OVERRIDE
         PUSH    KGDT_R0_CODE
@@ -489,11 +341,7 @@ Kbf40:
         RETF
 
 Kbf50:
-    ;
-    ; Return resulting context
-    ;
-
-        MOV     EAX, DWORD PTR [ESP+44+LocalStack]     ; (eax) = Context Record
+        MOV     EAX, DWORD PTR [ESP+44+LocalStack]
         POP     [EAX + CsEflags]
         POP     [EAX + CsEax]
         POP     [EAX + CsSegGs]
@@ -508,12 +356,9 @@ Kbf50:
         MOV     [EAX + CsEsi], ESI
         MOV     [EAX + CsEbp], EBP
 
-;
-; Restore regs & return
-;
-        DEC     _KiInBiosCall                         ; Clear  the 'In Bios' flag
+        DEC     _KiInBiosCall
 
-        ADD     ESP, LocalStack                                          ;remove scratch space
+        ADD     ESP, LocalStack
         POP     EDI
         POP     ESI
         POP     EBX
@@ -530,46 +375,21 @@ stdENDP _KeI386Call16BitFunction
 ;     IN PUCHAR Parameters,
 ;     IN ULONG Size
 ;     )
-;
-; Routine Description:
-;
-;     This function calls the 16 bit function which supports C style calling convention.
-;
-; Parameters:
-;
-;     EntryOffset and EntrySelector - specifies the entry point of the 16 bit function.
-;
-;     Parameters - supplies a pointer to a parameter block which will be
-;         passed to 16 bit function as parameters.
-;
-;     Size - supplies the size of the parameter block.
-;
-;   NOTE: Caller must be at DPC_LEVEL
-;
-; Returned Value:
-;
-;     AX returned by 16 bit function.
-;
 ;--
 
 cPublicProc _KeI386Call16BitCStyleFunction,4
 
-;
-;  verify CurrentIrql
-;  verify context flags
-;
-
-        PUSH    EBP                             ; save nonvolatile registers
+        PUSH    EBP
         PUSH    EBX
         PUSH    ESI
         PUSH    EDI
 
         COPY_CALL_FRAME _KiBiosFrame
 
-        INC     _KiInBiosCall                         ; Set the 'In Bios' flag
+        INC     _KiInBiosCall
 IFDEF DBG
-        CMP   _KiInBiosCall, 2
-        JB  @F
+        CMP     _KiInBiosCall, 2
+        JB      @F
         PUSH    OFFSET FLAT:KiBiosReenteredAssert
         CALL    _DbgPrint
         ADD     ESP, 4
@@ -577,68 +397,39 @@ IFDEF DBG
 ENDIF
 
         MOV     EDI, ESP
-        SUB     ESP, LocalStack          ;  now, add in some scratch space
-        MOV     ESI, DWORD PTR [ESP + LocalStack +28]       ; (esi)->BiosParameters
-        OR         ESI, ESI
-        JZ         SHORT @F
+        SUB     ESP, LocalStack
+        MOV     ESI, DWORD PTR [ESP + LocalStack + 28]
+        OR      ESI, ESI
+        JZ      SHORT @F
 
-        MOV    ECX, [ESP + LocalStack +32]                 ; (ecx) = parameter size
-        SUB    ESP, ECX                        ; allocate space on TOS to copy parameters
+        MOV     ECX, [ESP + LocalStack + 32]
+        SUB     ESP, ECX
 
-        MOV   EDI, ESP
-        REP     MOVSB                           ; (edi)-> Top of nonvolatile reg save area
-        ADD    EDI, LocalStack           ; edi now points to original stack
+        MOV     EDI, ESP
+        REP     MOVSB
+        ADD     EDI, LocalStack
 
 @@:
 
-;
-; We're using a 32bit CS:EIP - go to a 16bit CS:IP
-; Note the base of KiAbiosCallSelector is the flat address of _KiI386AbiosCall
-; routine.
-;
-
-;
-; Remap current stack to 16:16 stack.  The base of the 16bit stack selector is
-; the base of current kernel stack.
-;
-
-        STACK32_TO_STACK16                      ; Switch to 16bit stack
-
-;
-; Push return address from 16 bit function call to kernel
-;
+        STACK32_TO_STACK16
 
         PUSH    WORD PTR KGDT_CODE16
         PUSH    WORD PTR (OFFSET FLAT:Kbfex40 - OFFSET FLAT:_KiI386CallAbios@16)
 
-        PUSH    WORD PTR 0200h                  ; flags
-        PUSH    WORD PTR [EDI + 24 ]             ; entry selector
-        PUSH    WORD PTR [EDI + 20 ]             ; entry offset
+        PUSH    WORD PTR 0200h
+        PUSH    WORD PTR [EDI + 24]
+        PUSH    WORD PTR [EDI + 20]
 
-;
-; Switch to 16bit CS
-;
         PUSH    KGDT_CODE16
         PUSH    OFFSET FLAT:Kbfex30 - OFFSET FLAT:_KiI386CallAbios@16
         RETF
 
 Kbfex30:
-;
-; "call" to 16 bit function
-;
         IRETD
 
 Kbfex40:
-;
-; Save return value.
-;
-
         DB      OPERAND_OVERRIDE
         PUSH    EAX
-
-;
-; Restore Flat mode segment registers.
-;
 
         DB      OPERAND_OVERRIDE
         MOV     EAX, KGDT_R0_PCR
@@ -651,17 +442,7 @@ Kbfex40:
 
         XOR     EAX, EAX
 
-;
-; Switch back to 32 bit stack
-;
-
         STACK16_TO_STACK32
-
-;
-; Push the flat code selector followed by the return offset, then
-; execute a far return and we'll be back in the 32-bit code space.
-;
-
 
         DB      OPERAND_OVERRIDE
         PUSH    KGDT_R0_CODE
@@ -673,12 +454,9 @@ Kbfex40:
 Kbfex50:
         POP     EAX
 
-;
-; Restore regs & return
-;
-        DEC    _KiInBiosCall                         ; Clear  the 'In Bios' flag
+        DEC     _KiInBiosCall
 
-        MOV     ESP, EDI                                 ; Also removes the scratch space!
+        MOV     ESP, EDI
         POP     EDI
         POP     ESI
         POP     EBX
@@ -687,24 +465,12 @@ Kbfex50:
 
 stdENDP _KeI386Call16BitCStyleFunction
 
-;
-; BBT cannot instrument code between BBT_Exclude_Selector_Code_Begin and this label
-;
-
         PUBLIC  _BBT_Exclude_Selector_Code_End
 _BBT_Exclude_Selector_Code_End  EQU     $
         INT 3
 
-;;
-;; END - power_management
-;;
-;; ********************************************************
-
-
         PUBLIC  _KiEndOfCode16
 _KiEndOfCode16  EQU     $
-
-
 
 _TEXT   ENDS
         END
